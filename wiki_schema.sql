60b3c2e2aad674caa991a2d5cb8598dba8786a2d
Jack Ammerman <jwacooks@gmail.com>
2017-01-10 11:25:36 -0500

    Add files via upload

---
+++ wiki_schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE wiki (wd text, wp text, lcnum text, P1871 text, P1156 text, P1816 text, P724 text, P1669 text, P214 text, P1667 text, P402 text, P1960 text, P244 text, P345 text, P212 text, P3241 text, P760 text, P2671 text, P1649 text, P243 text, P672 text, P846 text, P1014 text, P3123 text, P236 text, P727 text, P830 text, P1417 text, P1144 text, P1157 text, P1184 text, P486 text, P2036 text, P245 text, P1566 text, P675 text, P1795 text, P1415 text, P957 text, P496 text, P1230 text, P2163 text);
+CREATE INDEX Idx1 on wiki(P244);
+CREATE INDEX Idx2 on wiki(P214);
+CREATE INDEX Idx3 on wiki(wd);
+
